64dc41f645584b078b111e55d760569a4633a3f5
Philip Gazagnes <philip.gazagnes@gmail.com>
2020-01-26 00:47:23 +0100

    nuxt implementation

---
+++ pages/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt-link to="/">back</nuxt-link>
+    <h1>TOTO</h1>
+    {{songData[0]}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  async asyncData({params}) {
+    const json = await import(`./../data/sc/${params.id}.json`);
+    return {
+      songData: json,
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Live notes ! Rock on !</h1>
+    <ul>
+      <li v-for="s in songs" :key="s.id">
+        <a :href="`/${s.id}`">{{s.name}} ({{s.artist}})</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import songsJson from './../data/songs.json';
+
+export default {
+  data() {
+    return {
+      songs: songsJson,
+    };
+  }
+}
+</script>
