f2feb4d5cc4b7e3e4c419bb66580431d202286c7
Philip <philip.gazagnes@gmail.com>
2022-07-13 22:18:28 +0200

    noindex

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,10 @@ export default {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1.0, user-scalable=no',
       },
+      {
+        name: 'robots',
+        content: 'noIndex',
+      },
     ],
     link: [
       {
